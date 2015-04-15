c62fd9f1086db6710eb8eba07d298d5f7c3dd9d1
17P <inagaki_mo@mti.co.jp>
2015-04-15 11:02:19 +0900

    [add] プロジェクト作成
    簡単なライブラリ用メソッド作成

---
+++ src/main/java/InagakiSampleLibrary.java
@@ -0,0 +1,13 @@
+
+public class InagakiSampleLibrary {
+	
+	public static final int inagakiAdd(int a, int b){
+		System.out.println("inagakiAdd! " + a + " + " + b);
+		return a + b;
+	}
+	
+	public static final int inagakiSub(int a, int b){
+		System.out.println("inagakiSub! " + a + " - " + b);
+		return a - b;
+	}
+}
